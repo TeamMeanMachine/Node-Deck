d0121e7202d284e8eb54baff6b75a583bbf1a98a
justin <justin@kilk.com>
2023-04-07 20:15:26 -1000

    networktable list with listeners

--- src/main/kotlin/org/team2471/frc/nodeDeck/NTTab.kt
+++ src/main/kotlin/org/team2471/frc/nodeDeck/NTTab.kt
@@ -1,14 +1,21 @@
 package org.team2471.frc.nodeDeck
 
+import edu.wpi.first.networktables.NetworkTableEvent
 import edu.wpi.first.networktables.NetworkTableInstance
+import edu.wpi.first.networktables.Topic
+import javafx.application.Platform
 import javafx.collections.FXCollections
 import javafx.collections.ObservableList
 import javafx.scene.control.ListView
 import javafx.scene.layout.StackPane
+import java.util.EnumSet
 
 object NTTab: StackPane() {
     var view: ListView<String>? = null
+    val instance = NetworkTableInstance.getDefault()
+    var usedTopics = arrayListOf<Topic>()
     var list: ObservableList<String>? = null
+    var assignedRow = 0
 
     init {
         list = FXCollections.observableArrayList()
@@ -16,12 +23,28 @@ object NTTab: StackPane() {
         NTTab.children.add(view)
     }
 
-    fun update() {
-        list?.clear()
-        NetworkTableInstance.getDefault().topics.forEach {
-            val t = NetworkTableInstance.getDefault().getEntry(it.name)
-            println("name: ${t.name} value: ${t.value.value}")
-            list?.add("name: ${t.name} value: ${t.value.value}")
+
+    fun checkIfNewTopics() {
+        val unusedTopics = usedTopics
+        instance.topics.forEach { topic ->
+            val tAssignedRow = if (assignedRow > (list?.size ?: 0)) list!!.size else assignedRow
+            assignedRow += 1
+            if (!usedTopics.contains(topic)) {
+                usedTopics.add(topic)
+                list?.add(tAssignedRow,"name: ${topic.name}      value: ${topic.getGenericEntry().get().value}")
+                instance.addListener(topic, EnumSet.of(NetworkTableEvent.Kind.kPublish, NetworkTableEvent.Kind.kValueAll)) { event: NetworkTableEvent ->
+                    println("name: ${topic.name} value: ${topic.getGenericEntry().get().value}")
+                    Platform.runLater {
+                        list?.removeAt(tAssignedRow)
+                        list?.add(tAssignedRow,"name: ${topic.name}      value: ${topic.getGenericEntry().get().value}")
+                    }
+                }
+            } else {
+                unusedTopics.drop(unusedTopics.indexOf(topic))
+            }
+        }
+        unusedTopics.forEach {
+            usedTopics.drop(usedTopics.indexOf(it))
         }
     }
 }
